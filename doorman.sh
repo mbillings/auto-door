81a9dc2e40921518918e6f7205fd56d5844077cd
mbillings <reil09@gmail.com>
2013-02-04 10:07:17 -0600

    +logic to lock/unlock based on wifi presence

--- doorman.sh
+++ doorman.sh
@@ -26,11 +26,11 @@
 # user-defined variables
 AVRDUDE="/bin/avrdude"
 arduino_type="m328p"
-avrdude_config="/etc/avrdude/avrdude.conv"
+avrdude_config="/etc/avrdude/avrdude.conf"
 baudrate="115200"
 hex_dir="hex"
 ip="192.168.1.1"
-mac_allow="/usr/local/etc/mac_allow"
+macs_here="/tmp/macs_here"
 port="/dev/ttyACM0"
 programmer="stk500v1"
 uri="Status_Wireless.asp"
@@ -41,14 +41,35 @@ userpass="TVPuppetPals!"
 declare -a macs
 macs=( AA:BB:CC:DD:EE:01 AA:BB:CC:DD:EE:02 )
 
+# if the macs_here file doesn't exist, touch it
+if [ ! -f "$macs_here" ]
+then touch "$macs_here"
+fi
+
+
+
 # loop every 10 seconds
 while :
 do
-  # since we don't care who corresponds to what mac address (at the moment), unlock whenever a new address is present
-  if [ $(curl --user $user:$pass $ip/$uri | egrep ^setWirelessTable | tr \' '\n' | grep ":" | egrep "$(echo ${#macs[@]} | tr ' ' '\|')" | wc -l) -le $(wc -l $mac_allow)]
-  then  "
-        "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex"/counterclockwise_unlock.cpp.hex
-  fi
-  
+  #
+
+  macs_connected=$(curl -s --user $username:$userpass $ip/$uri | egrep ^setWirelessTable | sed s#\'#\\n#g | grep ":" | egrep "$(echo ${macs[@]} | tr ' ' '\|') | tr '\n' ';'")
+
+  # if a MAC address is present, check if it's in the macs_here list. If not, unlock the door and add it!
+  for ((i=0;i<${#macs[@]};i++))
+  do if [ $(echo "$macs_connected" | grep "${macs["$i"]}" | wc -l) -eq 1  ]  # if the mac address is present in the router log,
+     then  if [ $(grep ${macs["$i"]} "$macs_here" | wc -l) -eq 0 ]  # and the mac address is not present in the file log,
+           then # unlock and add the mac address
+#                "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex"/clockwise_lock.cpp.hex
+                "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/counterclockwise_unlock.cpp.hex
+                echo ${macs["$i"]} >> "$macs_here"            
+           fi
+     else  if [ $(grep ${macs["$i"]} "$macs_here" | wc -l) -eq 1 ]  # if the mac is not present in the router logs, but the mac is present in the file log
+     	   then # lock and remove the mac address
+                "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/clockwise_lock.cpp.hex
+     	        sed -i $(grep -n ${macs["$i"]} "$macs_here" | tr ':' '\n' | head -1)d "$macs_here"
+     	   fi
+  	 fi
+  done
   sleep 10
 done
