0bd98b8833eaae9f0bbff4adccdf9fa9f64f4e25
mbillings <reil09@gmail.com>
2013-02-09 14:15:08 -0600

    ~renamed hex files to shorter, more obvious names; ~made changes in doorman script for hex name changes

--- doorman.sh
+++ doorman.sh
@@ -5,26 +5,31 @@
 # 
 #         USAGE: set a cron job to ensure ./doorman.sh is running
 # 
-#   DESCRIPTION: Lock/unlock door based on presence of mac address and current
-#                door status (unlocked/locked)
+#   DESCRIPTION: Lock/unlock door based on 
 # 
 #       OPTIONS: ---
-#  REQUIREMENTS: continuous servo (eventually 180 servo will be added)
+#  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: Tested with a Netgear dd-wrt'd router with mini firmware rev.24
-#                (FUTURE) unlock/lock based on time of day so it's easier to
-#                leave the house at 6:50 every morning and have the door
-#                lock at 7:00.
-#                0.3 avrdude is the program of choice.
+#                - (FUTURE) unlock/lock based on time of day so it's easier to
+#                  leave the house at 6:50 every morning and have the door
+#                  lock at 7:00.
+#                - avrdude is the program of choice for uploading hex files.
+#                - Recompiled hex files so lock resets to position 179,
+#                  but since there is no set position for unlock to fully
+#                  unlock the deadbolt, we set it to just move a certain amount
+#                  until the door is unlocked.
+#                  Because of this, we always reset to the locked position 
+#                  before unlocking.
+#                  This was done because we still want to be able to manually
+#                  lock/unlock the deadbolt if we're inside our house without
+#                  having to turn off/on our phone's wifi.
 #
 #  ORGANIZATION: ---
 #       CREATED: 27 January 2013
 #      REVISION: 0.1 Base functionality for unlocking when a new address appears
 #                    need to update to use script rather than IDE. Need to add 
 #                    touch file so it doesn't keep unlocking once a mac connects
-#                0.2 Touch file added, only unlocks/locks if locked/unlocked and
-#                    mac address is present/leaves. Also added reset_to_center
-#                    hex file if program just started up.
 #===============================================================================
 
 # ardunio and transfer layer variables
@@ -43,11 +48,10 @@ userpass="TVPuppetPals!"
 
 # OS variables
 hex_dir="/home/me/auto-door/hex"
-lock="clockwise_lock.cpp.hex"
 macs_here="/tmp/macs_here"
 poll_interval="5"
-reset_lock="reset_to_center.cpp.hex"
-unlock="counterclockwise_unlock.cpp.hex"
+reset_lock="reset_lock.cpp.hex"
+unlock="ccw_unlock.cpp.hex"
 
 # array of mac addresses to monitor (does this really need to be an array?)
 declare -a macs
@@ -65,11 +69,8 @@ echo "closed $(date --date="now" +%s)" > "$macs_here"  # put "closed" in the FIL
 # loop 
 while :
 do
-  #
-  echo ${macs[@]}
+  # get the current macs connected from the router page
   macs_connected=$(curl -s --user $username:$userpass $ip/$uri | egrep ^setWirelessTable | sed s#\'#\\n#g | grep ":" | egrep "$(echo ${macs[@]} | tr ' ' '\|') | tr '\n' ';'")
-  #macs_connected=$(curl -s --user $username:$userpass $ip/$uri | egrep ^setWirelessTable | sed s#\'#\\n#g | grep ":" | egrep "$(echo ${macs[@]} | tr ' ' '\|') | tr '\n' ';'")
-  echo macs_connected: $macs_connected
 
 # this will be consolidated into one if/then statement after initial testing
 
@@ -82,8 +83,9 @@ do
 
                status=$(grep "closed" "$macs_here" | wc -l)                 # check and see if the lock is currently in the closed position
                if [ "$status" -eq 1 ]                                       # if it is locked,
-               then                                                         # send the unlock hex file to the arduino to unlock it
-                     "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/"$unlock"
+               then                                                         # send the hex files to the servo to reset its position, and unlock
+                    "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/"$reset_lock"
+                    "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/"$unlock"
                      sed -i '/closed/d' "$macs_here"                        # delete the line containing "closed"
                      echo "open $(date --date="now" +%s)" >> "$macs_here"   # put "open" in the FILE log to change the status to unlocked
                fi
@@ -96,8 +98,8 @@ do
      	       
      	       status=$(grep "open" "$macs_here" | wc -l)                   # check if we are open (unlocked) 
                if [ "$status" -eq 1 ]                                       # if we are open (unlocked)
-               then                                                         # send the lock hex file to the arduino to lock it
-                    "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/"$lock"
+               then                                                         # send the reset_lock hex file to the arduino to lock it
+                    "$AVRDUDE" -C "$avrdude_config" -F -p "$arduino_type" -c "$programmer" -P "$port" -b "$baudrate" -D -Uflash:w:"$hex_dir"/"$reset_lock"
                     sed -i '/open/d' "$macs_here"                           # delete the line containing "open"
                     echo "closed $(date --date="now" +%s)" >> "$macs_here"  # put "closed" in the FILE log to change the status to unlocked
      	       fi
